885f878778b478d8b0fe515c1cdf544c8361d5a0
sofia bear <78060087+sophie177@users.noreply.github.com>
2023-08-18 11:04:31 -0700

    Update fnsDemo.py
    good to go

--- fnsDemo.py
+++ fnsDemo.py
@@ -1,4 +1,49 @@
 # Boeing Programming Camp 2023
-# Functions demo : Day 3
-# Lecture guide / activity to guide understanding of functions
+# Rapid Fire Prizes Quiz: Day 5
+
+a = 1   # what datatype is this? 
+b = 2 
+c = 3
+d = 0.14  # what datatype is this? 
+e = "elephant" # what datatype is this?
+f = True  # what datatype is this?
+
+if(a != 2):  # what is a conditional statement?
+  print("ardvark") # why do we indent here? 
+else: # when does else execute? (when if isn't true)
+  print("armadillo") 
+# what will print? 
+# change var values, now what will print
+
+def poopy(a, b, c):
+  newVal = a + b - c
+  return newVal
+# what is part is the input/argument?
+# what part is the output?
+# what is our result? 
+
+if(e == "elephant"):   # what will execute? 
+  poopy(10, 10, 10) 
+else:
+  poopy(3, 15, 9)
+# change conditional operator to != and ask what would execute now
+# bonus points if they can explain why 
+
+while(a < 6): # How many times will this execute?
+  print("loop")
+  a = a + 1 # what does this do? What is it called?
+
+word = ["f", "a", "r", "t"]
+# what is this?
+# what datatype is held in this list?
+
+for letter in word:
+  print(letter)
+# what will print?
+# What is the trigger/condition for the loop to end?  
+
+# why is a loop different from an if statement?
+# what is a similarity between a loop and an if statement? (hint: they both use a condition to determine when they execute)
+
+# When do we indent? (after an if, else, while loop, or for loop
 
